f916c9a971539828aefcecf9fddb9bc1e8021787
sunseehray <sabtirazona@byui.edu>
2025-03-05 10:51:16 +0800

    clean code and remove unused usgs js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-let graphicsLayer;
-
-// set template for popup
-const popupTemplate = {
-    title: "{Name}",
-    content: "{Description}"
-};
-
 // JavaScript Dates
 // http://www.w3schools.com/Jsref/jsref_obj_date.asp
 const today = new Date();
@@ -25,14 +17,12 @@ const yesterdayISO = yesterday.toISOString();
 // PARAMETERS: format to get geojson, starttime and endtime to get data from past 24 hours
 let apiUrl = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${yesterdayISO}&endtime=${todayISO}`;
 
+
+let graphicsLayer;
 // creating graphics layer and graphic point are based from arcgis tutorial
 function generateQuakeMap(filter) {
     require(["esri/Graphic", "esri/layers/GraphicsLayer"], (Graphic, GraphicsLayer) => {
         const arcgisMap = document.querySelector("arcgis-map");
-        // if (!arcgisMap) {
-        //     console.error("arcgisMap element not found!");
-        //     return;
-        // }
 
         if(!graphicsLayer) {
             graphicsLayer = new GraphicsLayer();
@@ -40,54 +30,60 @@ function generateQuakeMap(filter) {
         } else {
             graphicsLayer.removeAll();
         }
+
+        // set template for popup
+        const popupTemplate = {
+            title: "{Name}",
+            content: "{Description}"
+        };
     
-            // fetch data
-            fetch(apiUrl)
-                .then(response => response.json())
-                .then(data => {
-                    data.features.forEach(incidence => {
-                        // set longitude and latitude for points
-                        let [longitude, latitude] = incidence.geometry.coordinates;
-                        
-                        // set magnitude of incidence
-                        let magnitude = incidence.properties.mag;
-                        
-                        // create points based on lon, lat, and magnitude
-                        let point = new Graphic({
-                            geometry: { 
-                                type: "point", 
-                                longitude: longitude, 
-                                latitude: latitude 
-                            },
-                            symbol: { 
-                                type: "simple-marker", 
-                                color: "orange", 
-                                size: `${3 + magnitude * 2}px`, // modify size based on magnitude
-                                width: 1
-                            },
-                            attributes: {
-                                Name: `Magnitude: ${magnitude}`,
-                                Description: incidence.properties.place
-                            },
-                            popupTemplate: popupTemplate,
-                        });
-    
-                        // filter
-                        if (filter == "all") {
-                            graphicsLayer.add(point);
-                        }
-                        else if (filter == "greater" && magnitude >= 3.5) {
-                            graphicsLayer.add(point);
-                        }
-                        else if (filter == "less" && magnitude < 3.5) {
-                            graphicsLayer.add(point);
-                        }
+        // fetch data
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                data.features.forEach(incidence => {
+                    // set longitude and latitude for points
+                    let [longitude, latitude] = incidence.geometry.coordinates;
+                    
+                    // set magnitude of incidence
+                    let magnitude = incidence.properties.mag;
+                    
+                    // create points based on lon, lat, and magnitude
+                    let point = new Graphic({
+                        geometry: { 
+                            type: "point", 
+                            longitude: longitude, 
+                            latitude: latitude 
+                        },
+                        symbol: { 
+                            type: "simple-marker", 
+                            color: "orange", 
+                            size: `${3 + magnitude * 2}px`, // modify size based on magnitude
+                            width: 1
+                        },
+                        attributes: {
+                            Name: `Magnitude: ${magnitude}`,
+                            Description: incidence.properties.place
+                        },
+                        popupTemplate: popupTemplate,
                     });
-                })
-                .catch(error => console.error("Error fetching data:", error));
-    
-            });
-        }
+
+                    // filter
+                    if (filter == "all") {
+                        graphicsLayer.add(point);
+                    }
+                    else if (filter == "greater" && magnitude >= 3.5) {
+                        graphicsLayer.add(point);
+                    }
+                    else if (filter == "less" && magnitude < 3.5) {
+                        graphicsLayer.add(point);
+                    }
+                });
+            })
+            .catch(error => console.error("Error fetching data:", error));
+
+        });
+    }
 
 document.addEventListener("DOMContentLoaded", () => {
     const quakeFilter = document.getElementById("quakeFilter");
